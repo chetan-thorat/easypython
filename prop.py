d24bc8f0a2ecea4831b203cf7813866d8e5bda57
Chetan Thorat <90889118+chetan-thorat@users.noreply.github.com>
2024-03-14 14:08:23 +0530

    Add files via upload

---
+++ prop.py
@@ -0,0 +1,9 @@
+import os
+
+list1 = [8,9,3,5,2,1,19]
+list1.reverse()
+print("The reverse list is", list1)
+
+list2 = [91,23,2,324,12,345,32,2,6,8,5,3]
+list2.sort()
+print("The sorted list is", list2)
